725efe1ed9fe9445b73e0bce776a967ac8fa3fa2
Adarsh-22 <71167604+Adarsh-22@users.noreply.github.com>
2021-10-31 22:35:50 +0530

    Delete src directory

--- src/components/btn.js
+++
@@ -1,9 +0,0 @@
-const Btn = () => {
-    return ( 
-     <div className="flex-center">
-         <div className="btn flex-center">Start Career Roadmap</div>
-     </div>
-     );
-}
- 
-export default Btn;
