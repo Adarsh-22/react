3743961bd26dcda3dd96a11327f180adb05b4d43
Adarsh-22 <71167604+Adarsh-22@users.noreply.github.com>
2021-10-31 22:28:10 +0530

    Add files via upload

---
+++ src/components/btn.js
@@ -0,0 +1,9 @@
+const Btn = () => {
+    return ( 
+     <div className="flex-center">
+         <div className="btn flex-center">Start Career Roadmap</div>
+     </div>
+     );
+}
+ 
+export default Btn;
---
+++ src/components/topbar.js
@@ -0,0 +1,14 @@
+import '../index.css';
+
+const TopBar = () => {
+    return ( 
+        <div>
+            <div className="topbar">
+                <div className="topbar-left"></div>
+                <div className="topbar-right"></div>
+            </div>
+        </div>
+     );
+}
+ 
+export default TopBar;
